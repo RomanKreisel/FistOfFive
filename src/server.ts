065e21a0fbacfc469f7e772c69a4fe9a0ae5292b
Roman Kreisel <mail@romankreisel.de>
2018-09-16 09:45:15 +0200

    First commit

---
+++ src/server.ts
@@ -0,0 +1,35 @@
+import * as express from 'express';
+import * as http from 'http';
+import * as WebSocket from 'ws';
+
+const app = express();
+
+//initialize a simple http server
+const server = http.createServer(app);
+
+server.on('request', function(request) {
+
+    console.log('abc');
+});
+
+//initialize the WebSocket server instance
+const wss = new WebSocket.Server({ server });
+
+wss.on('connection', (ws: WebSocket) => {
+
+    //connection is up, let's add a simple simple event
+    ws.on('message', (message: string) => {
+
+        //log the received message and send it back to the client
+        console.log('received: %s', message);
+        ws.send(`Hello, you sent -> ${message}`);
+    });
+
+    //send immediatly a feedback to the incoming connection    
+    ws.send('Hi there, I am a WebSocket server');
+});
+
+//start our server
+server.listen(process.env.PORT || 8999, () => {
+    console.log(`Server started)`);
+});
